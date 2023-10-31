87f4abb390260bd78e48627f024934c20939beff
Gbogboade Ayomide <ay4allcrown@gmail.com>
2023-10-31 22:36:11 +0100

    Initial Commit

---
+++ ConversionApp/ConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionAppApp.swift
+//  ConversionApp
+//
+//  Created by Ayomide Gbogboade on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
