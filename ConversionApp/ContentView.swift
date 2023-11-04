dec63f8d4359164cff6d5d51e4190b9511a40d18
Gbogboade Ayomide <ay4allcrown@gmail.com>
2023-11-04 20:53:36 +0100

    feat: add conversion

--- ConversionApp/ContentView.swift
+++ ConversionApp/ContentView.swift
@@ -7,15 +7,107 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum TemperatureUnits {
+ case celsius, kelvin, fahrenheit
+}
+
+extension TemperatureUnits{
+    var symbol:  String{
+        switch self {
+        case .celsius: "C"
+        case .fahrenheit: "F"
+        case .kelvin: "K"
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var fromUnit = TemperatureUnits.celsius
+    @State private var toUnit = TemperatureUnits.fahrenheit
+    
+    @State private var value = 0.0
+    
+    var toValue: Double{
+        if toUnit == fromUnit {
+            return value
+        }
+        
+
+        
+        var result: Double = 0.0
+          
+          switch fromUnit {
+          case .celsius:
+              switch toUnit {
+              case .celsius:
+                  result = value
+              case .fahrenheit:
+                  result = (value * 9/5) + 32
+              case .kelvin:
+                  result = value + 273.15
+              }
+              
+          case .fahrenheit:
+              switch toUnit {
+              case .celsius:
+                  result = (value - 32) * 5/9
+              case .fahrenheit:
+                  result = value
+              case .kelvin:
+                  result = (value - 32) * 5/9 + 273.15
+              }
+              
+          case .kelvin:
+              switch toUnit {
+              case .celsius:
+                  result = value - 273.15
+              case .fahrenheit:
+                  result = (value - 273.15) * 9/5 + 32
+              case .kelvin:
+                  result = value
+              }
+          }
+          
+          return result
+    }
+    
+    let tempUnits: [TemperatureUnits] = [.celsius, .fahrenheit, .kelvin]
+   
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section("From"){
+                    Picker("Convert from", selection: $fromUnit){
+                        ForEach(tempUnits, id: \.self ){
+                            Text($0.symbol)
+                        }
+                    }.pickerStyle(.segmented)
+                   
+                    
+                    TextField("Enter value in ", value: $value, format: .number)
+                }
+                
+               ZStack {
+                   Color.blue
+                    Section("To"){
+                        VStack{
+                            Picker("Convert To", selection: $toUnit){
+                                ForEach(tempUnits, id: \.self ){
+                                    Text($0.symbol)
+                                }
+                            }.pickerStyle(.segmented)
+                            
+                            Text(toValue,format: .number)
+                        }
+                    }
+                }
+
+                
+                
+            }
+       
+            .navigationTitle("Temprature Converter")
+        }
     }
 }
 
